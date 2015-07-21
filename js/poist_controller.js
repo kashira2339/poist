cd1ec991e5b07dae08e95057bdd961a714d475f9
Taka SASAKI <tsukune2339@gmail.com>
2015-07-22 00:12:47 +0900

    いらないもの消す

--- js/poist_controller.js
+++ js/poist_controller.js
@@ -1,28 +1,9 @@
 (function(){
-
-    var x;
-    var y;
-    var relativeX;
-    var relativeY;
-
     var poist = new Poist('Hello', 'Hello world!!');
 
-    var isDragging = false;
-
-    poist.get.addEventListener('dragstart', function(e) {
-        relativeX = e.x - poist.position.x;
-        relativeY = e.y - poist.position.y;
-        isDragging = true;
-    });
-
     poist.get.addEventListener('dragend', function(e) {
-        poist.move(e.clientX+window.scrollX, e.clientY+window.scrollY);
-        isDragging = false;
+        poist.move(e.clientX+window.scrollX,
+                   e.clientY+window.scrollY);
     });
 
-    poist.get.addEventListener('mousemove', function(e) {
-        if (isDragging) {
-            poist.move(e.x, e.y);
-        }
-    });
 })();
