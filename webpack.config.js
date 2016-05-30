67aa7364984eb01bc198fa28df57a586558b0490
Taka SASAKI <tsukune2339@gmail.com>
2016-05-31 01:15:12 +0900

    use npm

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var webpack      = require('webpack'),
+    path         = require('path'),
+    precss       = require('precss'),
+    autoprefixer = require('autoprefixer');
+
+module.exports = {
+  entry: ['babel-polyfill', path.join(__dirname, 'js/index.js')],
+  output: {
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false },
+      output:   { comments: false }
+    }),
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loader: 'style!css!postcss!sass'
+      }
+    ]
+  },
+  postcss: function () {
+    return [precss, autoprefixer];
+  }
+};
